8275b1c7534eb1eba91336b99fa083ddd26452ad
Denka88 <animedenisik.com@mail.ru>
2025-03-27 22:07:16 +0300

    authorservice test complited

--- src/test/java/bip/online/biplio2023/impl/AuthorServiceImplTest.java
+++ src/test/java/bip/online/biplio2023/impl/AuthorServiceImplTest.java
@@ -2,22 +2,21 @@ package bip.online.biplio2023.impl;
 
 import bip.online.biplio2023.entity.Author;
 import bip.online.biplio2023.repo.AuthorRepo;
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
@@ -30,18 +29,35 @@ class AuthorServiceImplTest {
 
     @InjectMocks
     private AuthorServiceImpl authorService;
+    
+    /**
+     * Метод для тестирования поиска всех авторов
+     * @param lastName1 - Отчество автора 1
+     * @param name1 - Имя автора 1
+     * @param surname1 - Фамилия автора 1
+     * @param lastName2 - Отчество автора 2
+     * @param name2 - Имя автора 2
+     * @param surname2 - Фамилия автора 2
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "Иванович, Иван, Иванов, Петрович, Иван, Иванов",
+            "Владленович, Иван, Иванов, Головач, Лена, Иванов",
+            "Алексеевич, Ибрагим, Кандибобер, Родионович, Глеб, Степанов",
+            "Емельянов, Сергей, Александрович, Лукич, Игорь, Демидов",
+            "Матвеев, Мирон, Александрович, Павлов, Артур, Артемович"
+    })
+    void findAllAuthors(String lastName1, String name1, String surname1, String lastName2, String name2, String surname2) {
+        Author author1 = new Author(1L, lastName1, name1, surname1);
+        Author author2 = new Author(2L, lastName2, name2, surname2);
 
-    void setList() {
-        List<Author> authors = new ArrayList<>();
-        authors.add(new Author(1L, "Иванович", "Иван", "Иванов"));
-        authors.add(new Author(2L, "Петрович", "Иван", "Иванов"));
-        authors.add(new Author(3L, "Владленович", "Иван", "Иванов"));
-        authors.add(new Author(4L, "Головач", "Лена", "Иванов"));
-    }
-
-    @Test
-    void findAllAuthors() {
-
+        List<Author> authorList = Arrays.asList(author1, author2);        
+        
+        when(authorRepo.findAll()).thenReturn(authorList);
+        
+        List<Author> result = authorService.findAllAuthors();
+        
+        assertEquals(authorList, result);
     }
 
     /**
@@ -56,7 +72,8 @@ class AuthorServiceImplTest {
             "1, Иванович, Иван, Иванов",
             "2, Петрович, Иван, Иванов",
             "3, Владленович, Иван, Иванов",
-            "4, Головач, Лена, Иванов"
+            "4, Головач, Лена, Иванов",
+            "5, Алексеевич, Ибрагим, Кандибобер"
     })
     void findById(Long id, String lastName, String name, String surName) {
         Author author = new Author(id, lastName, name, surName);
@@ -64,10 +81,9 @@ class AuthorServiceImplTest {
         when(authorRepo.findById(id)).thenReturn(Optional.of(author));
 
         Optional<Author> result = authorService.findById(id);
-        System.out.printf("Результат: %s %s %s", result.get().getName(), result.get().getSurName(), result.get().getLastName());
 
         assertTrue(result.isPresent());
-        assertEquals("Петр", result.get().getName());
+        assertEquals(author.getName(), result.get().getName());
         verify(authorRepo, times(1)).findById(id);
     }
 
@@ -84,9 +100,10 @@ class AuthorServiceImplTest {
             "1, Иванович, Иван, Иванов",
             "2, Петрович, Иван, Иванов",
             "3, Владленович, Иван, Иванов",
-            "4, Головач, Лена, Иванов"
+            "4, Головач, Лена, Иванов",
+            "5, Алексеевич, Ибрагим, Кандибобер"
     })
-    void save(Long id, String name, String surName, String lastName) {
+    void save(Long id, String lastName, String name, String surName) {
         Author author = new Author(id, lastName, name, surName);
 
         when(authorRepo.save(author)).thenReturn(author);
@@ -96,4 +113,51 @@ class AuthorServiceImplTest {
         assertEquals(result, author);
     }
 
+
+    /**
+     * Метод для тестирования обновления автора в бд
+     * @param lastName - Отчество автора
+     * @param name - Имя автора
+     * @param surname - Фамилия автора
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "Иванович, Иван, Иванов",
+            "Владленович, Иван, Иванов",
+            "Алексеевич, Ибрагим, Кандибобер",
+            "Емельянов, Сергей, Александрович",
+            "Матвеев, Мирон, Александрович"
+    })
+    void update(String lastName, String name, String surname){
+        Author author = new Author(1L, lastName, name, surname);
+
+        authorService.update(author);
+
+        verify(authorRepo, times(1)).save(author);
+    }
+
+    /**
+     * Метод для тестирования удаления автора из бд
+     * @param id - Идентификатор автора
+     * @param lastName - Отчество автора
+     * @param name - Имя автора
+     * @param surname - Фамилия автора
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "1, Иванович, Иван, Иванов",
+            "2, Владленович, Иван, Иванов",
+            "3, Алексеевич, Ибрагим, Кандибобер",
+            "4, Емельянов, Сергей, Александрович",
+            "5, Матвеев, Мирон, Александрович"
+    })
+    void deleteById(Long id, String lastName, String name, String surname){
+        Author author = new Author(id, lastName, name, surname);
+
+        authorService.save(author);
+
+        authorService.deleteById(id);
+
+        assertFalse(authorRepo.existsById(id));
+    }
 }
---
+++ src/test/java/bip/online/biplio2023/impl/BookServiceImplTest.java
@@ -0,0 +1,49 @@
+﻿package bip.online.biplio2023.impl;
+
+import bip.online.biplio2023.entity.Book;
+import bip.online.biplio2023.repo.BookRepo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceImplTest {    
+    
+    @Mock
+    private BookRepo bookRepo;
+    
+    @InjectMocks
+    private BookServiceImpl bookService;
+    
+    @ParameterizedTest
+    @CsvSource({
+            ""
+    })
+    void findAllBooks() {
+        Book book1 = new Book(1L, );
+        Book book2 = new Book();
+        
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
